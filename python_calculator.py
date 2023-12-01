a5f1cda4aadc441a9b87f24d123503f10408fb82
Christian Roy S. Mabilin <145094099+xtianroy04@users.noreply.github.com>
2023-12-01 15:53:00 +0800

    Update python_calculator.py
    I added a GUI using tkinter

--- python_calculator.py
+++ python_calculator.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import messagebox
+
 def multiply(num1, num2):
     return num1 * num2
 
@@ -13,44 +16,61 @@ def divide(num1, num2):
     else:
         return "Cannot divide by zero."
 
-def main():
-    print("Select operation:")
-    print("1. Multiply")
-    print("2. Add")
-    print("3. Subtract")
-    print("4. Divide")
-    
+def perform_operation(choice, num1, num2):
+    if choice == 1:
+        return multiply(num1, num2)
+    elif choice == 2:
+        return add(num1, num2)
+    elif choice == 3:
+        return subtract(num1, num2)
+    elif choice == 4:
+        return divide(num1, num2)
+
+def on_submit():
     try:
-        choice = int(input("Enter choice (1/2/3/4): "))
+        choice = int(choice_var.get())
+        num1 = float(entry_num1.get())
+        num2 = float(entry_num2.get())
     except ValueError:
-        print("Error: Please enter a valid choice (1/2/3/4).")
+        messagebox.showerror("Error", "Invalid input. Please enter valid numbers and choice.")
         return
 
     if choice not in [1, 2, 3, 4]:
-        print("Error: Please enter a valid choice (1/2/3/4).")
-        return
-    
-    try:
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-    except ValueError:
-        print("Error: It must be numbers.")
+        messagebox.showerror("Error", "Invalid choice. Please enter a valid choice (1/2/3/4).")
         return
 
-    if choice == 1:
-        result = multiply(num1, num2)
-        print(f"{num1} * {num2} = {result}")
-    elif choice == 2:
-        result = add(num1, num2)
-        print(f"{num1} + {num2} = {result}")
-    elif choice == 3:
-        result = subtract(num1, num2)
-        print(f"{num1} - {num2} = {result}")
-    elif choice == 4:
-        result = divide(num1, num2)
-        print(f"{num1} / {num2} = {result}")
-    else:
-        print("Invalid input. Please enter a valid choice.")
+    result = perform_operation(choice, num1, num2)
+    result_label.config(text=f"Result: {result}")
+    messagebox.showinfo("Result", f"The result is: {result}")
 
-if __name__ == "__main__":
-    main()
+def stop_program():
+    root.destroy()
+
+# Create GUI
+root = tk.Tk()
+root.title("Calculator")
+root.geometry("400x300")  # Set the size of the GUI
+
+# Set font size
+font_size = ("Arial", 12)
+
+# Widgets
+choice_var = tk.StringVar()
+tk.Label(root, text="Select operation:", font=font_size).pack()
+
+operations = [("Multiply", 1), ("Add", 2), ("Subtract", 3), ("Divide", 4)]
+for text, value in operations:
+    tk.Radiobutton(root, text=text, variable=choice_var, value=value, font=font_size).pack()
+
+entry_num1 = tk.Entry(root, width=10, font=font_size)
+entry_num1.pack()
+entry_num2 = tk.Entry(root, width=10, font=font_size)
+entry_num2.pack()
+
+submit_button = tk.Button(root, text="Submit", command=on_submit, font=font_size)
+submit_button.pack()
+
+result_label = tk.Label(root, text="Result: ", font=font_size)
+result_label.pack()
+
+root.mainloop()
